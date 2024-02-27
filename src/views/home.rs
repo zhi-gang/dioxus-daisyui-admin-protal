230e1c9e63573d367e34f25a8e081f4f20e902dd
Zhigang <zhi-gang.yan@outlook.com>
2024-02-27 15:31:20 +0800

    msg bell

--- src/views/home.rs
+++ src/views/home.rs
@@ -40,9 +40,15 @@ pub fn Home(cx: Scope) ->Element {
                         }
                         h1{class:"text-2xl font-semibold ml-2","{(*menu_selection.read()).to_string()}"}
                     }
-                    
-                    
                     Theme {}
+                    button{class:"drawer-button btn btn-ghost ml-4  btn-circle",
+                        div{class:"indicator",
+                            svg{xmlns:"http://www.w3.org/2000/svg", fill:"none",  stroke:"currentColor" ,class:"h-5 inline-block w-5 stroke-2",
+                                path{style:"stroke-linecap:round;stroke-linejoin:round", d:"M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"}
+                            }
+                            span{class:"indicator-item badge badge-secondary badge-sm", "15"}
+                        }
+                    }
                 }
                 main{class:"flex-1 overflow-y-auto md:pt-4 pt-4 px-6  bg-base-200",
                     Outlet::<Route> {}
